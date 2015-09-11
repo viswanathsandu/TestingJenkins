c26a39b494bf1416e8b08fd569bf79101cbd9eea
vissu <vissu@Admins-MacBook-Pro-4.local>
2015-09-11 09:42:35 +0530

    Copied login and userprofile screens

--- app/src/main/java/com/education/corsalite/activities/UserProfileActivity.java
+++ app/src/main/java/com/education/corsalite/activities/UserProfileActivity.java
@@ -1,24 +1,26 @@
-package com.education.corsalite;
+package com.education.corsalite.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.education.corsalite.R;
+import com.education.corsalite.fragments.UserProfileDetailsFragment;
 
-public class MainActivity extends ActionBarActivity {
+public class UserProfileActivity extends AppCompatActivity implements UserProfileDetailsFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_profile);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_user_profile, menu);
         return true;
     }
 
@@ -35,5 +37,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
