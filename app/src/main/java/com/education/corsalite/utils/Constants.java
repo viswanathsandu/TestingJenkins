576c4085e800b57b6e9cd871549fb5fc3c1b1d42
Ayush Jha <ayush.jha@mutualmobile.com>
2015-09-15 23:41:52 +0530

    adding constants file

---
+++ app/src/main/java/com/education/corsalite/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.education.corsalite.utils;
+
+/**
+ * Created by ayush on 15/09/15.
+ */
+public class Constants {
+    public final static String FORGOT_PASSWORD_URL = "http://app.corsalite.com/v1/login/forgotPassword";
+}
+
