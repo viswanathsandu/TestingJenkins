f6238d797ba5f046ba9b5dbcceaa682d20dcddba
Aastha Maheshwari <aastha.maheshwari@myntra.com>
2015-10-10 20:43:07 +0530

    Refactoring analytics graphs

---
+++ app/src/main/java/com/education/corsalite/models/CustomLegend.java
@@ -0,0 +1,14 @@
+package com.education.corsalite.models;
+
+/**
+ * Created by Aastha on 10/10/15.
+ */
+public class CustomLegend {
+    public int color;
+    public String label;
+
+    public CustomLegend(int color,String label){
+        this.color = color;
+        this.label = label;
+    }
+}
