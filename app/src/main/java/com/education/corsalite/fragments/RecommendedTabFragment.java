1032b217902b9ad2ff8577485605e6dd6f4b19be
Viswanath <viswanath.sandu@olacabs.com>
2015-10-15 03:24:02 +0530

    Added course integration with Recommended Reading Tab

--- app/src/main/java/com/education/corsalite/fragments/RecommendedTabFragment.java
+++ app/src/main/java/com/education/corsalite/fragments/RecommendedTabFragment.java
@@ -13,18 +13,22 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.education.corsalite.R;
+import com.education.corsalite.activities.AbstractBaseActivity;
 import com.education.corsalite.adapters.RecommendationsAdapter;
 import com.education.corsalite.api.ApiCallback;
 import com.education.corsalite.api.ApiManager;
 import com.education.corsalite.cache.LoginUserCache;
 import com.education.corsalite.models.responsemodels.CorsaliteError;
+import com.education.corsalite.models.responsemodels.Course;
 import com.education.corsalite.models.responsemodels.CourseAnalysis;
 import com.education.corsalite.utils.L;
 
 import java.util.List;
+import java.util.zip.Inflater;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import de.greenrobot.event.EventBus;
 import retrofit.client.Response;
 
 /**
@@ -39,24 +43,43 @@ public class RecommendedTabFragment extends Fragment {
     @Bind(R.id.tv_failure_text)TextView mFailureText;
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mAdapter;
+    private LayoutInflater mInflater;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recommended_tab, container, false);
         ButterKnife.bind(this, view);
-
+        EventBus.getDefault().register(this);
         mRecyclerView.setVisibility(View.VISIBLE);
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
-        getCourseData(inflater);
+        this.mInflater = inflater;;
         return view;
-
     }
 
-    private void getCourseData(final LayoutInflater inflater) {
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(AbstractBaseActivity.selectedCourse != null) {
+            onEvent(AbstractBaseActivity.selectedCourse);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    public void onEvent(Course course) {
+        getCourseData(course.courseId.toString());
+    }
+
+    private void getCourseData(String courseId) {
         //passing static data TODO
-        ApiManager.getInstance(getActivity()).getCourseAnalysisData(LoginUserCache.getInstance().loginResponse.studentId, "13", null, "Topic", "None", "180", "true",
+        ApiManager.getInstance(getActivity()).getCourseAnalysisData(LoginUserCache.getInstance().loginResponse.studentId,
+                courseId, null, "Topic", "None", "180", "true",
                 new ApiCallback<List<CourseAnalysis>>() {
                     @Override
                     public void failure(CorsaliteError error) {
@@ -75,7 +98,7 @@ public class RecommendedTabFragment extends Fragment {
                             mProgressBar.setVisibility(View.GONE);
                             mRecyclerView.setVisibility(View.VISIBLE);
                             linearLayout.setVisibility(View.VISIBLE);
-                            mAdapter = new RecommendationsAdapter(courseAnalysisList, inflater);
+                            mAdapter = new RecommendationsAdapter(courseAnalysisList, mInflater);
                             mRecyclerView.setAdapter(mAdapter);
                         }
 
