94d6b82c0dca2fed512a3b2887229e528528091c
Girish Kumar <girish.kumar@myntra.com>
2015-10-16 02:48:18 +0530

    added next previous functionality

--- app/src/main/java/com/education/corsalite/models/ChapterModel.java
+++ app/src/main/java/com/education/corsalite/models/ChapterModel.java
@@ -1,6 +1,7 @@
 package com.education.corsalite.models;
 
 import com.education.corsalite.models.responsemodels.BaseModel;
+import com.google.gson.annotations.SerializedName;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -12,51 +13,17 @@ import java.util.SortedSet;
  */
 public class ChapterModel extends BaseModel implements Comparable<ChapterModel>{
 
+    @SerializedName("idCourseSubjectChapter")
     public String idChapter;
+    @SerializedName("ChapterName")
     public String chapterName;
+    @SerializedName("ChapterStatus")
     public String chapterStatus;
+    @SerializedName("ChapterSortOrder")
     public String chapterSortOrder;
-    public HashMap<String, SortedSet<TopicModel>> topicMap;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ChapterModel that = (ChapterModel) o;
-
-        if (idChapter != null ? !idChapter.equals(that.idChapter) : that.idChapter != null)
-            return false;
-        if (chapterName != null ? !chapterName.equals(that.chapterName) : that.chapterName != null)
-            return false;
-        if (chapterStatus != null ? !chapterStatus.equals(that.chapterStatus) : that.chapterStatus != null)
-            return false;
-        if (chapterSortOrder != null ? !chapterSortOrder.equals(that.chapterSortOrder) : that.chapterSortOrder != null)
-            return false;
-        return !(topicMap != null ? !topicMap.equals(that.topicMap) : that.topicMap != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = idChapter != null ? idChapter.hashCode() : 0;
-        result = 31 * result + (chapterName != null ? chapterName.hashCode() : 0);
-        result = 31 * result + (chapterStatus != null ? chapterStatus.hashCode() : 0);
-        result = 31 * result + (chapterSortOrder != null ? chapterSortOrder.hashCode() : 0);
-        result = 31 * result + (topicMap != null ? topicMap.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "ChapterModel{" +
-                "idChapter='" + idChapter + '\'' +
-                ", chapterName='" + chapterName + '\'' +
-                ", chapterStatus='" + chapterStatus + '\'' +
-                ", chapterSortOrder='" + chapterSortOrder + '\'' +
-                ", topicMap=" + topicMap +
-                '}';
-    }
+    @SerializedName("topics")
+    public List<TopicModel> topicMap;
 
     @Override
     public int compareTo(ChapterModel another) {
--- app/src/main/java/com/education/corsalite/models/SubjectModel.java
+++ app/src/main/java/com/education/corsalite/models/SubjectModel.java
@@ -1,6 +1,7 @@
 package com.education.corsalite.models;
 
 import com.education.corsalite.models.responsemodels.BaseModel;
+import com.google.gson.annotations.SerializedName;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -12,59 +13,19 @@ import java.util.SortedSet;
  */
 public class SubjectModel extends BaseModel {
 
+    @SerializedName("idCourseSubject")
     public String idSubject;
+    @SerializedName("SubjectName")
     public String subjectName;
+    @SerializedName("SubjectStatus")
     public String subjectStatus;
+    @SerializedName("ScoreRed")
     public String scoreRed;
+    @SerializedName("ScoreAmber")
     public String scoreAmber;
+    @SerializedName("ScoreLevelPassing")
     public String scoreLevelPassing;
-    public HashMap<String, SortedSet<ChapterModel>> chapterMap;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public List<ChapterModel> chapters;
 
-        SubjectModel that = (SubjectModel) o;
-
-        if (idSubject != null ? !idSubject.equals(that.idSubject) : that.idSubject != null)
-            return false;
-        if (subjectName != null ? !subjectName.equals(that.subjectName) : that.subjectName != null)
-            return false;
-        if (subjectStatus != null ? !subjectStatus.equals(that.subjectStatus) : that.subjectStatus != null)
-            return false;
-        if (scoreRed != null ? !scoreRed.equals(that.scoreRed) : that.scoreRed != null)
-            return false;
-        if (scoreAmber != null ? !scoreAmber.equals(that.scoreAmber) : that.scoreAmber != null)
-            return false;
-        if (scoreLevelPassing != null ? !scoreLevelPassing.equals(that.scoreLevelPassing) : that.scoreLevelPassing != null)
-            return false;
-        return !(chapterMap != null ? !chapterMap.equals(that.chapterMap) : that.chapterMap != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = idSubject != null ? idSubject.hashCode() : 0;
-        result = 31 * result + (subjectName != null ? subjectName.hashCode() : 0);
-        result = 31 * result + (subjectStatus != null ? subjectStatus.hashCode() : 0);
-        result = 31 * result + (scoreRed != null ? scoreRed.hashCode() : 0);
-        result = 31 * result + (scoreAmber != null ? scoreAmber.hashCode() : 0);
-        result = 31 * result + (scoreLevelPassing != null ? scoreLevelPassing.hashCode() : 0);
-        result = 31 * result + (chapterMap != null ? chapterMap.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "SubjectModel{" +
-                "idSubject='" + idSubject + '\'' +
-                ", subjectName='" + subjectName + '\'' +
-                ", subjectStatus='" + subjectStatus + '\'' +
-                ", scoreRed='" + scoreRed + '\'' +
-                ", scoreAmber='" + scoreAmber + '\'' +
-                ", scoreLevelPassing='" + scoreLevelPassing + '\'' +
-                ", chapterMap=" + chapterMap +
-                '}';
-    }
 }
