33207f8cacb036593f7ffa3f092b033e0f9dc38e
Viswanath <viswanath.sandu@olacabs.com>
2015-12-05 21:18:15 +0530

    Removed net connectivity logic from login activity. It has to be common in all screens

--- app/src/main/java/com/education/corsalite/activities/LoginActivity.java
+++ app/src/main/java/com/education/corsalite/activities/LoginActivity.java
@@ -19,7 +19,6 @@ import com.education.corsalite.models.responsemodels.CorsaliteError;
 import com.education.corsalite.models.responsemodels.LoginResponse;
 import com.education.corsalite.utils.Constants;
 import com.education.corsalite.utils.Encryption;
-import com.education.corsalite.utils.SystemUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -75,34 +74,30 @@ public class LoginActivity extends AbstractBaseActivity {
     }
 
     private void login(String username, String password) {
-        if(SystemUtils.isNetworkConnected(this)) {
-            showProgress();
-            ApiManager.getInstance(this).login(username, password, new ApiCallback<LoginResponse>(this) {
-                @Override
-                public void failure(CorsaliteError error) {
-                    super.failure(error);
-                    closeProgress();
-                    if (error != null && !TextUtils.isEmpty(error.message)) {
-                        showToast(error.message);
-                    }
+        showProgress();
+        ApiManager.getInstance(this).login(username, password, new ApiCallback<LoginResponse>(this) {
+            @Override
+            public void failure(CorsaliteError error) {
+                super.failure(error);
+                closeProgress();
+                if (error != null && !TextUtils.isEmpty(error.message)) {
+                    showToast(error.message);
                 }
+            }
 
-                @Override
-                public void success(LoginResponse loginResponse, Response response) {
-                    super.success(loginResponse, response);
-                    closeProgress();
-                    if (loginResponse.isSuccessful()) {
-                        ApiCacheHolder.getInstance().setLoginResponse(loginResponse);
-                        dbManager.saveReqRes(ApiCacheHolder.getInstance().login);
-                        onLoginsuccess(loginResponse);
-                    } else {
-                        showToast(getResources().getString(R.string.login_failed));
-                    }
+            @Override
+            public void success(LoginResponse loginResponse, Response response) {
+                super.success(loginResponse, response);
+                closeProgress();
+                if (loginResponse.isSuccessful()) {
+                    ApiCacheHolder.getInstance().setLoginResponse(loginResponse);
+                    dbManager.saveReqRes(ApiCacheHolder.getInstance().login);
+                    onLoginsuccess(loginResponse);
+                } else {
+                    showToast(getResources().getString(R.string.login_failed));
                 }
-            });
-        }else {
-            showToast("Please check your network connection");
-        }
+            }
+        });
     }
 
     private void onLoginsuccess(LoginResponse response) {
