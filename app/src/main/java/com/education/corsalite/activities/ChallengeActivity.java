b4961a52e22b502de6b25b350c39a3f9602d3f9d
viewercanure <viewer.canure@gmail.com>
2015-12-22 01:58:34 +0530

    Setup screen for challege test added.

--- app/src/main/java/com/education/corsalite/activities/ChallengeActivity.java
+++ app/src/main/java/com/education/corsalite/activities/ChallengeActivity.java
@@ -11,8 +11,15 @@ import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.education.corsalite.R;
+import com.education.corsalite.fragments.FriendsListFragment;
+import com.education.corsalite.fragments.TestSetupFragment;
+
+import java.io.Serializable;
+import java.util.List;
 
 public class ChallengeActivity extends AbstractBaseActivity {
+    private FriendsListCallback mFriendsListCallback;
+    private TestSetupCallback mTestSetupCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,26 @@ public class ChallengeActivity extends AbstractBaseActivity {
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         RelativeLayout myView = (RelativeLayout) inflater.inflate(R.layout.activity_challenge, null);
         frameLayout.addView(myView);
-
+        initListeners();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, FriendsListFragment.newInstance(mFriendsListCallback)).commit();
     }
 
+    private void initListeners() {
+        mFriendsListCallback = new FriendsListCallback() {
+            @Override
+            public void onNextClick(List<FriendsListFragment.FriendData> selectedFriends) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, TestSetupFragment.newInstance(mTestSetupCallback)).commit();
+            }
+        };
+    }
+
+    public interface FriendsListCallback extends Serializable {
+        public void onNextClick(List<FriendsListFragment.FriendData> selectedFriends);
+    }
+
+    public interface TestSetupCallback extends Serializable {
+
+    }
 }
--- app/src/main/java/com/education/corsalite/fragments/FriendsListFragment.java
+++ app/src/main/java/com/education/corsalite/fragments/FriendsListFragment.java
@@ -7,8 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.SearchView;
+import android.widget.TextView;
 
 import com.education.corsalite.R;
+import com.education.corsalite.activities.ChallengeActivity;
 import com.education.corsalite.adapters.DividerItemDecoration;
 import com.education.corsalite.adapters.FriendsAdapter;
 
@@ -20,20 +22,42 @@ import java.util.List;
  */
 public class FriendsListFragment extends BaseFragment  implements SearchView.OnQueryTextListener, SearchView.OnCloseListener {
 
+    private static final String ARG_CALLBACK = "ARG_CALLBACK";
     private SearchView mFriendSearchView;
     private RecyclerView mRecyclerView;
     private FriendsAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private TextView mTextViewCancel;
+    private TextView mTextViewNext;
+    ChallengeActivity.FriendsListCallback mFriendsListCallback;
 
-    public FriendsListFragment() {
+    public FriendsListFragment(){}
+
+
+    public static FriendsListFragment newInstance(ChallengeActivity.FriendsListCallback mFriendsListCallback) {
+        FriendsListFragment fragment = new FriendsListFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CALLBACK, mFriendsListCallback);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mFriendsListCallback = (ChallengeActivity.FriendsListCallback) getArguments().getSerializable(ARG_CALLBACK);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_challenge, container, false);
+        View view = inflater.inflate(R.layout.fragment_friends_list, container, false);
         mFriendSearchView = (SearchView) view.findViewById(R.id.sv_friend_search);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.rv_friends_list);
+        mTextViewCancel = (TextView) view.findViewById(R.id.tv_friendlist_cancel);
+        mTextViewNext = (TextView) view.findViewById(R.id.tv_friendlist_next);
 
         mRecyclerView.setHasFixedSize(true);
 
@@ -49,6 +73,13 @@ public class FriendsListFragment extends BaseFragment  implements SearchView.OnQ
 
         mFriendSearchView.setOnQueryTextListener(this);
         mFriendSearchView.setOnCloseListener(this);
+
+        mTextViewNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFriendsListCallback.onNextClick(new ArrayList<FriendData>());
+            }
+        });
         return view;
     }
 
