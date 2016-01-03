7eb3d3860d956ce68dc06fefba7256bab6ba3f56
Madhuri <madhuri.g@keeptraxinc.com>
2016-01-03 19:30:43 +0530

    Added splash activity

---
+++ app/src/main/java/com/education/corsalite/activities/SplashActivity.java
@@ -0,0 +1,7 @@
+package com.education.corsalite.activities;
+
+/**
+ * Created by Madhuri on 03-01-2016.
+ */
+public class SplashActivity {
+}
---
+++ app/src/main/java/com/education/corsalite/models/FeatureModel.java
@@ -0,0 +1,7 @@
+package com.education.corsalite.models;
+
+/**
+ * Created by Madhuri on 03-01-2016.
+ */
+public class FeatureModel {
+}
