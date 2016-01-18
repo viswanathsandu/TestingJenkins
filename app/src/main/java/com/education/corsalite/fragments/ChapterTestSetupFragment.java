373314714b599ccaa280cbe9519c094e3f265879
Meeth <meeth.dinesh@olacabs.com>
2016-01-18 21:18:42 +0530

    Fragment Dialog for Chapter Test

--- app/src/main/java/com/education/corsalite/fragments/ChapterTestSetupFragment.java
+++ app/src/main/java/com/education/corsalite/fragments/ChapterTestSetupFragment.java
@@ -6,11 +6,16 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.education.corsalite.R;
+import com.education.corsalite.activities.ExerciseActivity;
 import com.education.corsalite.activities.TestStartActivity;
 
 import java.util.ArrayList;
@@ -24,13 +29,18 @@ import butterknife.OnClick;
  *
  * @author Meeth D Jain
  */
-public class ChapterTestSetupFragment extends DialogFragment {
+public class ChapterTestSetupFragment extends DialogFragment implements AdapterView.OnItemSelectedListener {
 
-    @Bind(R.id.spinner_chapter_level)
-    Spinner mChapterLevelSpinner;
+    @Bind(R.id.checkbox_adaptive_learning)
+    CheckBox mAdaptiveLearningCheckbox;
     @Bind(R.id.edit_txt_chapter_test_setup_questions)
     EditText mNoOfQuestionsEditTxt;
+    @Bind(R.id.spinner_chapter_level)
+    Spinner mChapterLevelSpinner;
 
+    private Bundle mExtras;
+    private int mChapterLevel = 0;
+    private boolean mIsAdaptiveLearningEnabled;
     private ArrayList<String> mChapterLevels;
 
     public static ChapterTestSetupFragment newInstance(Bundle bundle) {
@@ -47,8 +57,8 @@ public class ChapterTestSetupFragment extends DialogFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Bundle bundle = getArguments();
-        mChapterLevels = bundle.getStringArrayList(TestStartActivity.EXTRAS_CHAPTER_LEVELS);
+        mExtras = getArguments();
+        mChapterLevels = mExtras.getStringArrayList(TestStartActivity.EXTRAS_CHAPTER_LEVELS);
     }
 
     @Nullable
@@ -57,12 +67,21 @@ public class ChapterTestSetupFragment extends DialogFragment {
         View rootView = inflater.inflate(R.layout.fragment_chapter_test_setup, container, false);
         ButterKnife.bind(this, rootView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, mChapterLevels);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, mChapterLevels);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mChapterLevelSpinner.setAdapter(adapter);
+        mChapterLevelSpinner.setOnItemSelectedListener(this);
 
         getDialog().setTitle("Test Option");
         getDialog().getWindow().setBackgroundDrawableResource(R.color.white);
+
+        mAdaptiveLearningCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mIsAdaptiveLearningEnabled = isChecked;
+                Toast.makeText(getActivity(), "Adaptive : " + mIsAdaptiveLearningEnabled, Toast.LENGTH_SHORT).show();
+            }
+        });
         return rootView;
     }
 
@@ -74,9 +93,21 @@ public class ChapterTestSetupFragment extends DialogFragment {
                 break;
             }
             case R.id.btn_next : {
+                startActivity(ExerciseActivity.getMyIntent(getActivity(), mExtras));
                 break;
             }
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        mChapterLevel = position;
+        Toast.makeText(getActivity(), "Selected : " + mChapterLevel, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
 
