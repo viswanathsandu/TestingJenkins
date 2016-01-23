73d389874b4ae064535b542debc7622bad068b4a
Meeth <meeth.dinesh@olacabs.com>
2016-01-23 20:05:05 +0530

    Scheduled Test Code

--- app/src/main/java/com/education/corsalite/fragments/TestScheduledFragment.java
+++ app/src/main/java/com/education/corsalite/fragments/TestScheduledFragment.java
@@ -1,11 +1,16 @@
 package com.education.corsalite.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -13,9 +18,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.education.corsalite.R;
+import com.education.corsalite.activities.ExerciseActivity;
 import com.education.corsalite.api.ApiCallback;
 import com.education.corsalite.api.ApiManager;
 import com.education.corsalite.cache.LoginUserCache;
+import com.education.corsalite.holders.IconTreeItemHolder;
 import com.education.corsalite.models.responsemodels.CorsaliteError;
 import com.education.corsalite.models.responsemodels.ScheduledTest;
 import com.education.corsalite.utils.L;
@@ -24,8 +31,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
-import butterknife.BindDimen;
 import butterknife.ButterKnife;
+import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 /**
@@ -33,7 +40,7 @@ import retrofit.client.Response;
  *
  * @author Meeth D Jain
  */
-public class TestScheduledFragment extends BaseFragment {
+public class TestScheduledFragment extends BaseFragment implements AdapterView.OnItemClickListener {
 
     @Bind(R.id.ll_container)
     LinearLayout mContainerLayout;
@@ -71,8 +78,29 @@ public class TestScheduledFragment extends BaseFragment {
         mAdapter = new ScheduledTestAdapter();
         mListViewScheduledTests.setAdapter(mAdapter);
 
-        //getScheduledTests();
+        getScheduledTests();
         return rootView;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        final ScheduledTest scheduledTest = mScheduledTestList.get(position);
+        Dialog dialog = new AlertDialog.Builder(getActivity())
+                .setTitle("Start " + scheduledTest.examName)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(getActivity(), ExerciseActivity.class));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        dialog.show();
     }
 
     private void getScheduledTests() {
@@ -82,6 +110,14 @@ public class TestScheduledFragment extends BaseFragment {
                     public void failure(CorsaliteError error) {
                         super.failure(error);
                         L.error(error.message);
+                        mProgressBar.setVisibility(View.GONE);
+                        mFailureTextView.setText("Sorry, couldn't fetch data");
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        super.failure(error);
+                        L.error(error.getMessage());
                         mProgressBar.setVisibility(View.GONE);
                         mFailureTextView.setText("Sorry, couldn't fetch data");
                     }
