d2619b8dd49b1da74d0787b7522cd7a04549d7f1
Madhuri <madhuri.g@keeptraxinc.com>
2016-01-24 17:10:08 +0530

    Added methods for posting user events

---
+++ app/src/main/java/com/education/corsalite/event/ContentReadingEvent.java
@@ -0,0 +1,9 @@
+package com.education.corsalite.event;
+
+/**
+ * Created by Madhuri on 24-01-2016.
+ */
+public class ContentReadingEvent {
+    public String id;
+    public String pageView;
+}
--- app/src/main/java/com/education/corsalite/event/UpdateUserEvents.java
+++ app/src/main/java/com/education/corsalite/event/UpdateUserEvents.java
@@ -4,9 +4,15 @@ import android.app.Activity;
 
 import com.education.corsalite.api.ApiCallback;
 import com.education.corsalite.api.ApiManager;
+import com.education.corsalite.cache.LoginUserCache;
 import com.education.corsalite.models.requestmodels.UserEventsModel;
 import com.education.corsalite.models.responsemodels.UserEventsResponse;
+import com.education.corsalite.utils.SystemUtils;
 import com.google.gson.Gson;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import retrofit.client.Response;
 
@@ -16,12 +22,61 @@ import retrofit.client.Response;
 public class UpdateUserEvents {
 
     public void postUserEvent(Activity activity, UserEventsModel model){
-        ApiManager.getInstance(activity).postUserEvents(new Gson().toJson(model), new ApiCallback<UserEventsResponse>(activity) {
-            @Override
-            public void success(UserEventsResponse userEventsResponse, Response response) {
-                super.success(userEventsResponse, response);
+        if(SystemUtils.isNetworkConnected(activity)) {
+            ApiManager.getInstance(activity).postUserEvents(new Gson().toJson(model), new ApiCallback<UserEventsResponse>(activity) {
+                @Override
+                public void success(UserEventsResponse userEventsResponse, Response response) {
+                    super.success(userEventsResponse, response);
 
-            }
-        });
+                }
+            });
+        }else {
+
+        }
     }
+
+    public void postContentReading(Activity activity,String eventSourceId,String pageView){
+        UserEventsModel model = getUserEventsModel("Content Reading",getDate(System.currentTimeMillis()),
+                getDate(System.currentTimeMillis()), eventSourceId,pageView);
+        postUserEvent(activity, model);
+    }
+
+    public void postExerciseAns(Activity activity, String eventSourceId, String pageView){
+        UserEventsModel model = getUserEventsModel("Exercise Answering",getDate(System.currentTimeMillis()),
+                getDate(System.currentTimeMillis()), eventSourceId,pageView);
+        postUserEvent(activity,model);
+    }
+
+    public void postTakingTest(Activity activity,String eventSourceId,String pageView){
+        UserEventsModel model = getUserEventsModel("Taking Test",getDate(System.currentTimeMillis()),
+                getDate(System.currentTimeMillis()), eventSourceId,pageView);
+        postUserEvent(activity,model);
+    }
+
+    public void postForumPosting(Activity activity,String eventSourceId,String pageView){
+        UserEventsModel model = getUserEventsModel("Forum Posting",getDate(System.currentTimeMillis()),
+                getDate(System.currentTimeMillis()), eventSourceId,pageView);
+        postUserEvent(activity,model);
+    }
+
+    private UserEventsModel getUserEventsModel(String eventName,String eventStartTime,
+                                               String eventEndTime, String eventSourceId, String pageView){
+        UserEventsModel model = new UserEventsModel();
+        model.userId = LoginUserCache.getInstance().loginResponse.studentId;
+        model.eventEndTime = eventEndTime;
+        model.eventStartTime =eventStartTime;
+        model.eventName = eventName;
+        model.eventSourceId = eventSourceId;
+        model.pageView = pageView;
+
+        return model;
+    }
+
+    private String getDate(long millis) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-mm-dd hh:mm:ss", Locale.getDefault());
+        Date date = new Date(millis);
+        return dateFormat.format(date);
+    }
+
 }
