fba161741b3dd1cb9a40fefafc74f655d2c7d6d3
Viswanath <viswanath.sandu@olacabs.com>
2016-02-04 16:05:25 +0530

    Fixed compilation error

--- app/src/main/java/com/education/corsalite/utils/AppUtils.java
+++ app/src/main/java/com/education/corsalite/utils/AppUtils.java
@@ -1,18 +1,8 @@
 package com.education.corsalite.utils;
 
-import android.content.Context;
-
-import com.education.corsalite.cache.LoginUserCache;
-
 /**
  * Created by vissu on 2/4/16.
  */
 public class AppUtils {
 
-    public static String getLoginUrl(Context context) {
-        LoginUserCache cache = LoginUserCache.getInstance();
-        cache.getLongResponse()
-
-        return "";
-    }
 }
