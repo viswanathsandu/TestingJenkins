bcd7659bb1c97007d53fd6aea8373bb3a930a326
Madhuri <madhuri.g@keeptraxinc.com>
2016-03-01 20:47:38 +0530

    Added interface to listen delete of offline test

--- app/src/main/java/com/education/corsalite/activities/OfflineContentActivity.java
+++ app/src/main/java/com/education/corsalite/activities/OfflineContentActivity.java
@@ -131,5 +131,8 @@ public class OfflineContentActivity extends AbstractBaseActivity {
 
     }
 
+    public interface IOfflineTestEventListener{
+        void onDeleteOfflineTest(int position,String tag);
+    }
 
 }
