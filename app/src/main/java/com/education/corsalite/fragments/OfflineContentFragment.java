390aab6134eafadb6077888e791ee7b596d9817e
Viswanath <viswanath.sandu@olacabs.com>
2016-03-26 15:52:56 +0530

    Fixed video downloading issue. Disabled navigation drawer in video screen

--- app/src/main/java/com/education/corsalite/fragments/OfflineContentFragment.java
+++ app/src/main/java/com/education/corsalite/fragments/OfflineContentFragment.java
@@ -26,6 +26,7 @@ import com.education.corsalite.models.responsemodels.Course;
 import com.education.corsalite.utils.AppPref;
 import com.education.corsalite.utils.Constants;
 import com.education.corsalite.utils.FileUtilities;
+import com.education.corsalite.utils.L;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.unnamed.b.atv.model.TreeNode;
@@ -43,13 +44,15 @@ import retrofit.client.Response;
 /**
  * Created by Aastha on 28/11/15.
  */
-public class OfflineContentFragment extends BaseFragment  implements OfflineContentActivity.IOfflineEventListener{
+public class OfflineContentFragment extends BaseFragment implements OfflineContentActivity.IOfflineEventListener {
 
-    @Bind(R.id.main_node_content) RelativeLayout mainNodeLayout;
-    @Bind(R.id.no_content_txt) View emptyContentView;
+    @Bind(R.id.main_node_content)
+    RelativeLayout mainNodeLayout;
+    @Bind(R.id.no_content_txt)
+    View emptyContentView;
 
     private AndroidTreeView tView;
-    private List<OfflineContent> offlineContentList ;
+    private List<OfflineContent> offlineContentList;
     private List<ExerciseOfflineModel> offlineExercises;
     private String selectedCourse;
     private String subjectId = "";
@@ -66,7 +69,7 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_offline_content, container, false);
         ButterKnife.bind(this, view);
-        if(getActivity() instanceof OfflineContentActivity) {
+        if (getActivity() instanceof OfflineContentActivity) {
             ((OfflineContentActivity) getActivity()).setOfflineListener(this);
         }
         return view;
@@ -75,7 +78,7 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
     private void getExercises(final Course course) {
         showProgress();
         emptyContentView.setVisibility(View.GONE);
-        DbManager.getInstance(getActivity().getApplicationContext()).getOfflineExerciseModels(course.courseId+"",
+        DbManager.getInstance(getActivity().getApplicationContext()).getOfflineExerciseModels(course.courseId + "",
                 new ApiCallback<List<ExerciseOfflineModel>>(getActivity()) {
                     @Override
                     public void success(List<ExerciseOfflineModel> exerciseOfflineModels, Response response) {
@@ -112,19 +115,19 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
                 String savedData = AppPref.getInstance(getActivity()).getValue("DATA_IN_PROGRESS");
                 ArrayList<OfflineContent> contents = new Gson().fromJson(savedData, new TypeToken<ArrayList<OfflineContent>>() {
                 }.getType());
-                if(contents != null) {
+                if (contents != null) {
                     offlineContents.addAll(contents);
                     getTopicIds(contents);
                 }
                 offlineContentList = new ArrayList<>();
-                for(OfflineContent offlineContent: offlineContents) {
-                    if(offlineContent.courseId.equalsIgnoreCase(course.courseId.toString())) {
+                for (OfflineContent offlineContent : offlineContents) {
+                    if (offlineContent.courseId.equalsIgnoreCase(course.courseId.toString())) {
                         offlineContentList.add(offlineContent);
                     }
                 }
-                if(offlineContentList != null && !offlineContentList.isEmpty()){
+                if (offlineContentList != null && !offlineContentList.isEmpty()) {
                     initNodes();
-                }else{
+                } else {
                     mainNodeLayout.removeAllViews();
                     emptyContentView.setVisibility(View.VISIBLE);
                 }
@@ -134,7 +137,7 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
 
     private void getTopicIds(ArrayList<OfflineContent> contents) {
         contentIds = new ArrayList<>();
-        for(OfflineContent offlineContent : contents){
+        for (OfflineContent offlineContent : contents) {
             contentIds.add(offlineContent.contentId);
         }
     }
@@ -147,12 +150,13 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
         DbManager.getInstance(getActivity()).saveContentIndexList(jsonObject, courseId, LoginUserCache.getInstance().loginResponse.studentId);
     }*/
 
-    private void updateContentIndexResponses(String contentId){
-        if(contentIds != null){
+    private void updateContentIndexResponses(String contentId) {
+        if (contentIds != null) {
             contentIds.remove(contentId);
             initNodes();
         }
     }
+
     @Override
     public void onCourseIdSelected(Course course) {
         getExercises(course);
@@ -167,51 +171,51 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
 
     private void initNodes() {
         TreeNode root = TreeNode.root();
-        for (OfflineContent offlineContent: offlineContentList) {
-            TreeNode subjectRoot =null;
-            for(TreeNode subjectNode:root.getChildren()) {
+        for (OfflineContent offlineContent : offlineContentList) {
+            TreeNode subjectRoot = null;
+            for (TreeNode subjectNode : root.getChildren()) {
                 if (((IconTreeItemHolder.IconTreeItem) subjectNode.getValue()).id.equalsIgnoreCase(offlineContent.subjectId)) {
-                    subjectRoot = subjectNode ;
+                    subjectRoot = subjectNode;
                     break;
                 }
 
             }
-            if(subjectRoot == null){
-                subjectRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_subject_white, offlineContent.subjectName,offlineContent.subjectId,"subject",false));
+            if (subjectRoot == null) {
+                subjectRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_subject_white, offlineContent.subjectName, offlineContent.subjectId, "subject", false));
                 root.addChild(subjectRoot);
             }
 
-            TreeNode chapterRoot =null;
-            for(TreeNode chapterNode:subjectRoot.getChildren()) {
-                if (((IconTreeItemHolder.IconTreeItem)chapterNode.getValue()).id.equalsIgnoreCase(offlineContent.chapterId)) {
-                    chapterRoot = chapterNode ;
+            TreeNode chapterRoot = null;
+            for (TreeNode chapterNode : subjectRoot.getChildren()) {
+                if (((IconTreeItemHolder.IconTreeItem) chapterNode.getValue()).id.equalsIgnoreCase(offlineContent.chapterId)) {
+                    chapterRoot = chapterNode;
                     break;
                 }
             }
-            if(chapterRoot == null){
+            if (chapterRoot == null) {
                 boolean showProgress = contentIds != null && contentIds.contains(offlineContent.contentId);
-                chapterRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_chapter,  offlineContent.chapterName,offlineContent.chapterId,"chapter",showProgress));
+                chapterRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_chapter, offlineContent.chapterName, offlineContent.chapterId, "chapter", showProgress));
                 subjectRoot.addChild(chapterRoot);
             }
 
-            TreeNode topicRoot =null;
-            boolean showProgress ;
-            for(TreeNode topicNode:chapterRoot.getChildren()) {
-                if (((IconTreeItemHolder.IconTreeItem)topicNode.getValue()).id.equalsIgnoreCase(offlineContent.topicId)) {
-                    topicRoot = topicNode ;
+            TreeNode topicRoot = null;
+            boolean showProgress;
+            for (TreeNode topicNode : chapterRoot.getChildren()) {
+                if (((IconTreeItemHolder.IconTreeItem) topicNode.getValue()).id.equalsIgnoreCase(offlineContent.topicId)) {
+                    topicRoot = topicNode;
                     break;
                 }
             }
-            if(topicRoot == null){
+            if (topicRoot == null) {
                 showProgress = contentIds != null && contentIds.contains(offlineContent.contentId);
-                topicRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_chapter, offlineContent.topicName,offlineContent.topicId,"topic",showProgress));
+                topicRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_chapter, offlineContent.topicName, offlineContent.topicId, "topic", showProgress));
                 chapterRoot.addChild(topicRoot);
 
                 // Add exercise as the first item in topic
                 List<ExerciseOfflineModel> addedList = new ArrayList<>();
-                for(ExerciseOfflineModel exercise : offlineExercises) {
-                    if(exercise.topicId.equals(offlineContent.topicId)) {
-                        IconTreeItemHolder.IconTreeItem item = new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_topics, "Exercise - " + exercise.topicId, exercise.topicId+"-"+exercise.courseId, "Exercise", false);
+                for (ExerciseOfflineModel exercise : offlineExercises) {
+                    if (exercise.topicId.equals(offlineContent.topicId)) {
+                        IconTreeItemHolder.IconTreeItem item = new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_topics, "Exercise - " + exercise.topicId, exercise.topicId + "-" + exercise.courseId, "Exercise", false);
                         item.setData(exercise);
                         addedList.add(exercise);
                         topicRoot.addChild(new TreeNode(item));
@@ -220,16 +224,16 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
                 offlineExercises.removeAll(addedList);
             }
 
-            TreeNode contentRoot =null;
-            for(TreeNode contentNode:topicRoot.getChildren()) {
-                if(((IconTreeItemHolder.IconTreeItem)contentNode.getValue()).id.equalsIgnoreCase(offlineContent.contentId)) {
-                    contentRoot = contentNode ;
+            TreeNode contentRoot = null;
+            for (TreeNode contentNode : topicRoot.getChildren()) {
+                if (((IconTreeItemHolder.IconTreeItem) contentNode.getValue()).id.equalsIgnoreCase(offlineContent.contentId)) {
+                    contentRoot = contentNode;
                     break;
                 }
             }
-            if(contentRoot == null){
+            if (contentRoot == null) {
                 showProgress = contentIds != null && contentIds.contains(offlineContent.contentId);
-                contentRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_topics,offlineContent.fileName, offlineContent.contentId,"content",showProgress));
+                contentRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ico_offline_topics, offlineContent.fileName, offlineContent.contentId, "content", showProgress));
                 topicRoot.addChild(contentRoot);
             }
         }
@@ -248,22 +252,25 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
         @Override
         public void onClick(TreeNode node, Object value) {
             IconTreeItemHolder.IconTreeItem item = (IconTreeItemHolder.IconTreeItem) value;
-            if(item.tag.equalsIgnoreCase("subject")
-                    || item.tag.equalsIgnoreCase("chapter")
-                    || item.tag.equalsIgnoreCase("topic")){
+
+            if(item.tag.equalsIgnoreCase("subject")){
                 subjectId = item.id;
                 subjectName = item.text;
-            } else if(item.tag.equalsIgnoreCase("content")) {
-                if(item.text.endsWith("html"))
-                    startContentActivity(topicId, chapterId,subjectId,item.id,item.text);
-                else
-                    startVideoActivity(course.name,subjectName,chapterName,topicName,item.text);
-            } else {
-                startExerciseTest(item.data instanceof ExerciseOfflineModel ? (ExerciseOfflineModel) item.data : null);
-
+            } else if(item.tag.equalsIgnoreCase("chapter")){
+                chapterId = item.id;
+                chapterName = item.text;
+            } else if(item.tag.equalsIgnoreCase("topic")){
+                topicId = item.id;
+                topicName = item.text;
+            } else if (item.tag.equalsIgnoreCase("content")) {
+                if (item.text.endsWith("html")) {
+                    startContentActivity(topicId, chapterId, subjectId, item.id, item.text);
+                } else {
+                    startVideoActivity(course.name, subjectName, chapterName, topicName, item.text);
+                }
+            } else if(item.data != null && item.data instanceof ExerciseOfflineModel){
+                startExerciseTest((ExerciseOfflineModel) item.data);
             }
-
-
         }
     };
 
@@ -275,37 +282,37 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
     };
 
     @Override
-    public void onDeleteOfflineData(String id,String tag){
+    public void onDeleteOfflineData(String id, String tag) {
         String path = null;
         ArrayList<OfflineContent> removeList = new ArrayList<>();
-        for(OfflineContent offlineContent : offlineContentList){
-            if(offlineContent.courseId.equalsIgnoreCase(selectedCourse)) {
+        for (OfflineContent offlineContent : offlineContentList) {
+            if (offlineContent.courseId.equalsIgnoreCase(selectedCourse)) {
                 switch (tag) {
                     case "subject":
                         if (id.equalsIgnoreCase(offlineContent.subjectId)) {
-                            path = offlineContent.courseName+"/"+offlineContent.subjectName;
+                            path = offlineContent.courseName + "/" + offlineContent.subjectName;
                             removeList.add(offlineContent);
                         }
                         break;
                     case "chapter":
                         if (id.equalsIgnoreCase(offlineContent.chapterId)) {
-                            path = offlineContent.courseName+"/"+offlineContent.subjectName+"/"+offlineContent.chapterName;
+                            path = offlineContent.courseName + "/" + offlineContent.subjectName + "/" + offlineContent.chapterName;
                             removeList.add(offlineContent);
                         }
                         break;
                     case "topic":
                         if (id.equalsIgnoreCase(offlineContent.topicId)) {
-                            path = offlineContent.courseName+"/"+offlineContent.subjectName+"/"+offlineContent.chapterName+"/"+offlineContent.topicName;
+                            path = offlineContent.courseName + "/" + offlineContent.subjectName + "/" + offlineContent.chapterName + "/" + offlineContent.topicName;
                             removeList.add(offlineContent);
                         }
                         break;
                     case "content":
                         if (id.equalsIgnoreCase(offlineContent.contentId)) {
-                            String pathPrefix = offlineContent.courseName + "/" + offlineContent.subjectName + "/" + offlineContent.chapterName + "/" + offlineContent.topicName ;
-                            if(offlineContent.fileName.split(Pattern.quote("."))[1].equalsIgnoreCase("video")) {
-                                path = pathPrefix + "/"+ "Video" + "/"+ offlineContent.fileName;
-                            }else{
-                                path = pathPrefix + "/" + "Html" + "/"+ offlineContent.fileName;
+                            String pathPrefix = offlineContent.courseName + "/" + offlineContent.subjectName + "/" + offlineContent.chapterName + "/" + offlineContent.topicName;
+                            if (offlineContent.fileName.split(Pattern.quote("."))[1].equalsIgnoreCase("video")) {
+                                path = pathPrefix + "/" + "Video" + "/" + offlineContent.fileName;
+                            } else {
+                                path = pathPrefix + "/" + "Html" + "/" + offlineContent.fileName;
                             }
                             removeList.add(offlineContent);
                         }
@@ -320,13 +327,13 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
         DbManager.getInstance(getActivity().getApplicationContext()).deleteOfflineContent(removeList);
 
         //Update treeview
-        for(OfflineContent offlineContent :removeList){
+        for (OfflineContent offlineContent : removeList) {
             offlineContentList.remove(offlineContent);
         }
         mainNodeLayout.removeAllViews();
-        if(offlineContentList.isEmpty()) {
+        if (offlineContentList.isEmpty()) {
             Toast.makeText(getActivity(), "No offline content available ", Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             initNodes();
         }
     }
@@ -340,29 +347,29 @@ public class OfflineContentFragment extends BaseFragment  implements OfflineCont
         startActivity(intent);
     }
 
-    private void startContentActivity(String topicId, String chapterId,String subjectId,String contentId,String contentName) {
+    private void startContentActivity(String topicId, String chapterId, String subjectId, String contentId, String contentName) {
         Intent intent = new Intent(getActivity(), ContentReadingActivity.class);
         intent.putExtra("courseId", AbstractBaseActivity.selectedCourse.courseId.toString());
         intent.putExtra("subjectId", subjectId);
         intent.putExtra("chapterId", chapterId);
         intent.putExtra("topicId", topicId);
         intent.putExtra("contentId", contentId);
-        intent.putExtra("contentName",contentName);
+        intent.putExtra("contentName", contentName);
         getActivity().startActivity(intent);
     }
 
-    private void startVideoActivity(String name,String mSubjectName,String mChapterName,String topicName,String contentName){
-        String folderStructure =  name + File.separator +mSubjectName + File.separator +
-                mChapterName + File.separator + topicName;
+    private void startVideoActivity(String name, String mSubjectName, String mChapterName, String topicName, String contentName) {
+        String folderStructure = name + File.separator + mSubjectName + File.separator + mChapterName + File.separator + topicName;
         File SDCardRoot = Environment.getExternalStorageDirectory();
-        File outDir = new File(SDCardRoot.getAbsolutePath() + File.separator + Constants.PARENT_FOLDER + File.separator +folderStructure);
+        File outDir = new File(SDCardRoot.getAbsolutePath() + File.separator + Constants.PARENT_FOLDER + File.separator + folderStructure);
         File file = new File(outDir.getAbsolutePath() + File.separator + Constants.VIDEO_FOLDER);
-        File newFile = new File(file,contentName);
+        File newFile = new File(file, contentName);
         Intent intent = new Intent(getActivity(), VideoActivity.class);
         intent.putExtra("videopath", newFile.getAbsolutePath());
-        if(newFile.exists()){
+        L.debug("Loading file from : "+newFile.getAbsolutePath());
+        if (newFile.exists()) {
             getActivity().startActivity(intent);
-        }else
+        } else
             showToast("File does not exist");
     }
 
