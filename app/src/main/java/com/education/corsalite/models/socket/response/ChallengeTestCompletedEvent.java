e33b6faa23e32935ecd76be92186756848577b98
Viswanath <viswanath.sandu@olacabs.com>
2016-04-05 21:41:54 +0530

    Fixed compilation issues

--- app/src/main/java/com/education/corsalite/models/socket/response/ChallengeTestCompletedEvent.java
+++ app/src/main/java/com/education/corsalite/models/socket/response/ChallengeTestCompletedEvent.java
@@ -5,14 +5,14 @@ import com.google.gson.annotations.SerializedName;
 /**
  * Created by vissu on 4/1/16.
  */
-public class ChallengeTestCompleteEvent {
+public class ChallengeTestCompletedEvent {
     public String event;
     @SerializedName("ChallengeTestParentID")
     public String challengeTestParentId;
     @SerializedName("ChallengerName")
     public String challengerName;
 
-    public ChallengeTestCompleteEvent(String event, String challengeTestParentId, String challengeName) {
+    public ChallengeTestCompletedEvent(String event, String challengeTestParentId, String challengeName) {
         this.event = event;
         this.challengeTestParentId = challengeTestParentId;
         this.challengerName = challengeName;
