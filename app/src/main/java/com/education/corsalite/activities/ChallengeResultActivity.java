62da41e306a02f4139f81168011b6565a29661a9
MADHURIKONGARA\Madhuri <kongara.madhuri@gmail.com>
2016-04-07 09:22:39 +0530

    Created layout for challenge result

---
+++ app/src/main/java/com/education/corsalite/activities/ChallengeResultActivity.java
@@ -0,0 +1,15 @@
+package com.education.corsalite.activities;
+
+import android.os.Bundle;
+
+import com.education.corsalite.R;
+
+/**
+ * Created by Madhuri on 07-04-2016.
+ */
+public class ChallengeResultActivity extends AbstractBaseActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.challenge_result);
+    }
+}
