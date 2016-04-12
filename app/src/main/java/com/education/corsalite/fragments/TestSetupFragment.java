ba9b57beb669beb2989ed512782f3bcd41c80308
MADHURIKONGARA\Madhuri <kongara.madhuri@gmail.com>
2016-04-12 23:14:09 +0530

    added label and hint to the edit text feilds

--- app/src/main/java/com/education/corsalite/fragments/TestSetupFragment.java
+++ app/src/main/java/com/education/corsalite/fragments/TestSetupFragment.java
@@ -64,7 +64,7 @@ public class TestSetupFragment extends BaseFragment {
     public TextView selectSubjError;
     @Bind(R.id.tv_timeInMinsError)
     public TextView timeInMinsError;
-    @Bind(R.id.tv_virCurrencyError)
+
     public TextView virCurrencyError;
 
     private List<SubjectModel> subjectList;
