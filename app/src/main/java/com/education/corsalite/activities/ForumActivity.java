92ea159530b26afc157f11db30d2b7c56652c0fd
Viswanath <viswanath.sandu@olacabs.com>
2016-04-15 12:05:25 +0530

    Fixed offline login issue when app is killed. Reverted the new post click event

--- app/src/main/java/com/education/corsalite/activities/ForumActivity.java
+++ app/src/main/java/com/education/corsalite/activities/ForumActivity.java
@@ -1,7 +1,6 @@
 package com.education.corsalite.activities;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -15,6 +14,7 @@ import com.education.corsalite.adapters.PostPagerAdapter;
 import com.education.corsalite.api.ApiCallback;
 import com.education.corsalite.api.ApiManager;
 import com.education.corsalite.cache.LoginUserCache;
+import com.education.corsalite.fragments.EditorDialogFragment;
 import com.education.corsalite.models.responsemodels.CorsaliteError;
 import com.education.corsalite.models.responsemodels.Course;
 import com.education.corsalite.models.responsemodels.CourseData;
@@ -31,8 +31,10 @@ import retrofit.client.Response;
  */
 public class ForumActivity extends AbstractBaseActivity {
 
-    @Bind(R.id.tabLayout) TabLayout mTabLayout;
-    @Bind(R.id.viewPager) ViewPager mViewPager;
+    @Bind(R.id.tabLayout)
+    TabLayout mTabLayout;
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
 
     private LayoutInflater inflater;
     private CourseData mCourseData;
@@ -84,12 +86,14 @@ public class ForumActivity extends AbstractBaseActivity {
                 });
     }
 
-        public void onNewPostClicked() {
-            Bundle bundle = new Bundle();
-            bundle.putString("type", "Forum");
-            bundle.putString("operation", "Add");
-            Intent post = new Intent(this,NewPostActivity.class);
-            post.putExtras(bundle);
-            startActivity(post);
-}}
+    public void onNewPostClicked() {
+        EditorDialogFragment fragment = new EditorDialogFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("type", "Forum");
+        bundle.putString("operation", "Add");
+        fragment.setArguments(bundle);
+        fragment.show(getSupportFragmentManager(), "ForumEditorDialog");
+    }
+
+}
 
