3b4b44c8746d7e0fc48a525e71705eff8189f316
pkandagatla <pkandagatla@pkandagatla.roam.corp.google.com>
2016-04-16 17:18:39 +0530

    result activity network settings

--- app/src/main/java/com/education/corsalite/adapters/ExamHistoryAdapter.java
+++ app/src/main/java/com/education/corsalite/adapters/ExamHistoryAdapter.java
@@ -18,13 +18,14 @@ import butterknife.ButterKnife;
 /**
  * Created by Aastha on 27/11/15.
  */
-public class ExamHistoryAdapter  extends AbstractRecycleViewAdapter{
+public class ExamHistoryAdapter extends AbstractRecycleViewAdapter {
 
     private SetOnExamHistoryClick setOnExamHistoryClick;
 
     public interface SetOnExamHistoryClick {
         void onItemClick(int position);
     }
+
     LayoutInflater inflater;
 
     public ExamHistoryAdapter(List<ExamHistory> examHistoryList, LayoutInflater inflater, SetOnExamHistoryClick setOnExamHistoryClick) {
@@ -45,18 +46,24 @@ public class ExamHistoryAdapter  extends AbstractRecycleViewAdapter{
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ((ExamHistoryViewHolder) holder).bindData(position, (ExamHistory)getItem(position));
+        ((ExamHistoryViewHolder) holder).bindData(position, (ExamHistory) getItem(position));
     }
 
     public class ExamHistoryViewHolder extends RecyclerView.ViewHolder {
 
         View parent;
-        @Bind(R.id.tv_examhistory_date)TextView examHistoryDate;
-        @Bind(R.id.tv_exam)TextView exam;
-        @Bind(R.id.tv_type)TextView type;
-        @Bind(R.id.tv_rank)TextView rank;
-        @Bind(R.id.tv_status)TextView status;
-        @Bind(R.id.tv_action)TextView action;
+        @Bind(R.id.tv_examhistory_date)
+        TextView examHistoryDate;
+        @Bind(R.id.tv_exam)
+        TextView exam;
+        @Bind(R.id.tv_type)
+        TextView type;
+        @Bind(R.id.tv_rank)
+        TextView rank;
+        @Bind(R.id.tv_status)
+        TextView status;
+        @Bind(R.id.tv_action)
+        TextView action;
 
         public ExamHistoryViewHolder(View view) {
             super(view);
@@ -66,20 +73,21 @@ public class ExamHistoryAdapter  extends AbstractRecycleViewAdapter{
 
         public void bindData(final int position, final ExamHistory examHistory) {
             // different color for alternate rows
-            if((position+1)% 2 == 0) {
+            if ((position + 1) % 2 == 0) {
                 parent.setBackgroundColor(inflater.getContext().getResources().getColor(R.color.tab_recycler_alternate_row));
             } else {
                 parent.setBackgroundColor(inflater.getContext().getResources().getColor(R.color.white));
             }
 
-            if(!examHistory.dateTime.isEmpty()) {
+            if (!examHistory.dateTime.isEmpty()) {
                 this.examHistoryDate.setText(examHistory.dateTime.split(" ")[0]);
             }
             this.exam.setText(examHistory.examName);
             this.type.setText(examHistory.testType);
             this.rank.setText(examHistory.rank);
             this.status.setText(examHistory.status);
-            action.setOnClickListener(new View.OnClickListener() {
+
+            parent.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     setOnExamHistoryClick.onItemClick(position);
