3558157b59209a6deca5516559629c6a831ccc4a
pkandagatla <pkandagatla@pkandagatla.roam.corp.google.com>
2016-04-16 18:13:56 +0530

    recommendation activity

--- app/src/main/java/com/education/corsalite/fragments/RecommendedTabFragment.java
+++ app/src/main/java/com/education/corsalite/fragments/RecommendedTabFragment.java
@@ -1,6 +1,7 @@
 package com.education.corsalite.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +15,9 @@ import android.widget.TextView;
 
 import com.education.corsalite.R;
 import com.education.corsalite.activities.AbstractBaseActivity;
+import com.education.corsalite.activities.ContentReadingActivity;
+import com.education.corsalite.activities.LoginActivity;
+import com.education.corsalite.activities.WebviewActivity;
 import com.education.corsalite.adapters.RecommendationsAdapter;
 import com.education.corsalite.api.ApiCallback;
 import com.education.corsalite.api.ApiManager;
@@ -22,6 +26,8 @@ import com.education.corsalite.models.responsemodels.CorsaliteError;
 import com.education.corsalite.models.responsemodels.Course;
 import com.education.corsalite.models.responsemodels.CourseAnalysis;
 import com.education.corsalite.utils.L;
+import com.education.corsalite.utils.SystemUtils;
+import com.education.corsalite.utils.WebUrls;
 
 import java.util.List;
 
@@ -33,15 +39,18 @@ import retrofit.client.Response;
 /**
  * Created by Aastha on 27/09/15.
  */
-public class RecommendedTabFragment extends Fragment {
+public class RecommendedTabFragment extends Fragment implements RecommendationsAdapter.SetOnRecommendationClickListener {
 
     @Bind(R.id.rv_analytics_recommended)
     RecyclerView mRecyclerView;
-    @Bind(R.id.progress_bar_tab)ProgressBar mProgressBar;
-    @Bind(R.id.headerLayout)LinearLayout linearLayout;
-    @Bind(R.id.tv_failure_text)TextView mFailureText;
+    @Bind(R.id.progress_bar_tab)
+    ProgressBar mProgressBar;
+    @Bind(R.id.headerLayout)
+    LinearLayout linearLayout;
+    @Bind(R.id.tv_failure_text)
+    TextView mFailureText;
     private RecyclerView.LayoutManager mLayoutManager;
-    private RecyclerView.Adapter mAdapter;
+    private RecommendationsAdapter mAdapter;
     private LayoutInflater mInflater;
 
     @Nullable
@@ -53,14 +62,15 @@ public class RecommendedTabFragment extends Fragment {
         mRecyclerView.setVisibility(View.VISIBLE);
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
-        this.mInflater = inflater;;
+        this.mInflater = inflater;
+        ;
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if(AbstractBaseActivity.selectedCourse != null) {
+        if (AbstractBaseActivity.selectedCourse != null) {
             onEvent(AbstractBaseActivity.selectedCourse);
         }
     }
@@ -85,7 +95,7 @@ public class RecommendedTabFragment extends Fragment {
                     @Override
                     public void failure(CorsaliteError error) {
                         super.failure(error);
-                        if(getActivity() == null) {
+                        if (getActivity() == null) {
                             return;
                         }
                         L.error(error.message);
@@ -96,14 +106,14 @@ public class RecommendedTabFragment extends Fragment {
                     @Override
                     public void success(List<CourseAnalysis> courseAnalysisList, Response response) {
                         super.success(courseAnalysisList, response);
-                        if(getActivity() == null) {
+                        if (getActivity() == null) {
                             return;
                         }
                         if (courseAnalysisList != null) {
                             mProgressBar.setVisibility(View.GONE);
                             mRecyclerView.setVisibility(View.VISIBLE);
                             linearLayout.setVisibility(View.VISIBLE);
-                            mAdapter = new RecommendationsAdapter(courseAnalysisList, mInflater);
+                            mAdapter = new RecommendationsAdapter(courseAnalysisList, mInflater, RecommendedTabFragment.this);
                             mRecyclerView.setAdapter(mAdapter);
                         }
 
@@ -111,5 +121,17 @@ public class RecommendedTabFragment extends Fragment {
                 });
 
 
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        CourseAnalysis courseAnalysis = (CourseAnalysis) mAdapter.getItem(position);
+        if (SystemUtils.isNetworkConnected(getActivity())) {
+            Intent intent = new Intent(getActivity(), ContentReadingActivity.class);
+            intent.putExtra("subjectId", "" + courseAnalysis.idCourseSubject);
+            intent.putExtra("chapterId", "" + courseAnalysis.idCourseSubjectChapter);
+            intent.putExtra("topicId", "" + courseAnalysis.idTopic);
+            startActivity(intent);
+        }
     }
 }
