762a526706c8fd0a941362217723a91c95d947c1
Madhuri <madhuri.g@keeptraxinc.com>
2016-05-26 22:56:35 +0530

    Added basic changes for new challenge test

--- app/src/main/java/com/education/corsalite/activities/ChallengeActivity.java
+++ app/src/main/java/com/education/corsalite/activities/ChallengeActivity.java
@@ -15,7 +15,6 @@ import com.education.corsalite.api.ApiCallback;
 import com.education.corsalite.api.ApiManager;
 import com.education.corsalite.cache.LoginUserCache;
 import com.education.corsalite.fragments.ChallengeTestRequestDialogFragment;
-import com.education.corsalite.fragments.FriendsListFragment;
 import com.education.corsalite.fragments.TestSetupFragment;
 import com.education.corsalite.helpers.WebSocketHelper;
 import com.education.corsalite.models.responsemodels.CreateChallengeResponseModel;
@@ -51,6 +50,7 @@ public class ChallengeActivity extends AbstractBaseActivity {
     @Bind(R.id.player2_txt) TextView player2_txt;
     @Bind(R.id.player3_txt) TextView player3_txt;
     @Bind(R.id.player4_txt) TextView player4_txt;
+    @Bind(R.id.tvInviteFriends) TextView inviteFriends;
 
     private TestSetupCallback mTestSetupCallback;
     public String mChallengeTestId = "";
@@ -73,8 +73,14 @@ public class ChallengeActivity extends AbstractBaseActivity {
         }
         if(!TextUtils.isEmpty(screenType)) {
             if(screenType.equalsIgnoreCase("NEW_CHALLENGE")) {
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragment_container, FriendsListFragment.newInstance(mFriendsListCallback), "FriendsList").commit();
+                inviteFriends.setVisibility(View.VISIBLE);
+                inviteFriends.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+               // getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, FriendsListFragment.newInstance(mFriendsListCallback), "FriendsList").commit();
             } else if(screenType.equalsIgnoreCase("REQUEST")) {
                 String challengeTestRequestJson = bundle.getString("challenge_test_request_json");
                 ChallengeTestRequestEvent event = new Gson().fromJson(challengeTestRequestJson, ChallengeTestRequestEvent.class);
---
+++ app/src/main/java/com/education/corsalite/activities/InviteFriendsActivity.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2013-2016 KeepTrax, Inc. All Rights Reserved.
+ *  NOTICE:  All information contained herein is, and remains the property of
+ *  KeepTrax Incorporated.  The intellectual and technical concepts contained
+ *  herein are proprietary to KeepTrax Incorporated and may be covered by U.S.
+ *  and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this
+ *  material as a whole or in part is strictly forbidden unless prior written
+ *  permission is obtained from KeepTrax Incorporated.
+ */
+
+package com.education.corsalite.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.education.corsalite.R;
+
+public class InviteFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_friends);
+    }
+}
