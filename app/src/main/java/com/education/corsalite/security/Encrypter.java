67fcaa2127646b5bc0d02bfa3cbcf03ae4db49f8
Viswanath <viswanath.sandu@olacabs.com>
2016-07-31 00:36:32 +0530

    Integrated AESHelper to enable lossless encryption and decryption

--- app/src/main/java/com/education/corsalite/security/Encrypter.java
+++ app/src/main/java/com/education/corsalite/security/Encrypter.java
@@ -1,15 +1,11 @@
 package com.education.corsalite.security;
 
-import android.util.Base64;
-
 import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -35,42 +31,12 @@ public class Encrypter {
 
     public static String encrypt(String password, String text)
             throws Exception {
-        try {
-            SecretKeySpec skeySpec = getKey(password);
-            byte[] clearText = text.getBytes("UTF8");
-
-            final byte[] iv = new byte[16];
-            Arrays.fill(iv, (byte) 0x00);
-            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
-
-            String encrypedValue = Base64.encodeToString(
-                    cipher.doFinal(clearText), Base64.DEFAULT);
-            return encrypedValue;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
+        return AESHelper.encrypt(password, text);
     }
 
-    public static String decrypt(String password, String data)
+    public static String decrypt(String password, String encryptedData)
             throws Exception {
-        try {
-            SecretKeySpec skeySpec = getKey(password);
-            byte[] encrypted = Base64.decode(data, Base64.DEFAULT);
-            final byte[] iv = new byte[16];
-            Arrays.fill(iv, (byte) 0x00);
-            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivParameterSpec);
-            String encrypedValue = new String(cipher.doFinal(encrypted));
-            return encrypedValue;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
+        return AESHelper.encrypt(password, encryptedData);
     }
 
 
