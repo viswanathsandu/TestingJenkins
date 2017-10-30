883ac301f0ec858bc76691575fc99904d53a7e6e
Viswanath Kumar Sandu <vissu.info@gmail.com>
2017-10-30 13:34:06 +0530

    Fixed crash

--- app/src/main/java/com/education/corsalite/activities/ContentReadingActivity.java
+++ app/src/main/java/com/education/corsalite/activities/ContentReadingActivity.java
@@ -267,14 +267,18 @@ public class ContentReadingActivity extends AbstractBaseActivity {
 
     private void addToForum(String htmlText) {
         Bundle bundle = new Bundle();
-        bundle.putString("type", "Forum");
-        bundle.putString("operation", "Add");
-        bundle.putString("student_id", LoginUserCache.getInstance().getLoginResponse().studentId);
-        bundle.putString("subject_id", subjectModelList.get(spSubject.getSelectedItemPosition()).idSubject);
-        bundle.putString("chapter_id", chapterModelList.get(spChapter.getSelectedItemPosition()).idChapter);
-        bundle.putString("topic_id", topicModelList.get(spTopic.getSelectedItemPosition()).idTopic);
-        bundle.putString("content_id", contentModelList.get(mContentIdPosition).idContent);
-        bundle.putString("content", htmlText);
+        try {
+            bundle.putString("type", "Forum");
+            bundle.putString("operation", "Add");
+            bundle.putString("student_id", LoginUserCache.getInstance().getLoginResponse().studentId);
+            bundle.putString("subject_id", subjectModelList.get(spSubject.getSelectedItemPosition()).idSubject);
+            bundle.putString("chapter_id", chapterModelList.get(spChapter.getSelectedItemPosition()).idChapter);
+            bundle.putString("topic_id", topicModelList.get(spTopic.getSelectedItemPosition()).idTopic);
+            bundle.putString("content_id", contentModelList.get(mContentIdPosition).idContent);
+            bundle.putString("content", htmlText);
+        } catch (Exception e) {
+            L.error(e.getMessage(), e);
+        }
         Intent intent = new Intent(this, EditorActivity.class);
         intent.putExtras(bundle);
         startActivity(intent);
